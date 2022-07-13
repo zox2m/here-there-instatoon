df693ec28de3a19c706239f5ffdde6b49c176bdf
zox2m <jumin0913@naver.com>
2022-07-13 21:05:18 +0900

    last

--- MBTI/js/share.js
+++ MBTI/js/share.js
@@ -1,15 +1,15 @@
-const url = 'https://instatoon-here-there.netlify.app/';
+const url = 'https://instatoon-here-there.netlify.app';
 
 function setShare(){
   var resultImg = document.querySelector('#resultImg');
   var resultAlt = resultImg.firstElementChild.alt;
   const shareTitle = '여기저기 인생게임';
   var shareDes = infoList_here[resultAlt].name;
-  var shareImage = url + '/img/here-' + resultAlt + '.png';
+  var shareImage = url + '/img/heres-' + resultAlt + '.png';
   var shareURL = url + '/page/here-' + resultAlt + '.html';
   
   if(isThere ==1){
-    shareImage = url + '/img/there-' + resultAlt + '.png';
+    shareImage = url + '/img/theres-' + resultAlt + '.png';
     shareURL = url + '/page/there-' + resultAlt + '.html';
     shareDes = infoList_there[resultAlt].name;
   }
